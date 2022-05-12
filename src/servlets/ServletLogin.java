dcbc3dc6f9c23ae59ba93a884ea783b0d9bacc5f
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2022-05-12 10:47:34 -0300

    Fazendo simulação de login no sistema

--- src/servlets/ServletLogin.java
+++ src/servlets/ServletLogin.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.catalina.Session;
+
 import model.ModelLogin;
 
 
@@ -42,6 +44,21 @@ public class ServletLogin extends HttpServlet {
 				
 				modelLogin.setLogin(login);
 				modelLogin.setSenha(senha);
+				
+				if(modelLogin.getLogin().equalsIgnoreCase("admin") && modelLogin.getSenha().equalsIgnoreCase("admin")) {
+					
+					request.getSession().setAttribute("usuario", modelLogin.getLogin());
+					
+					RequestDispatcher dispatcher = request.getRequestDispatcher("principal/principal.jsp");
+					dispatcher.forward(request, response);
+					
+				}
+				else {
+					
+					RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+					request.setAttribute("msg", "Informe Login e/ou Senha corretamente");
+					dispatcher.forward(request, response);
+				}
 			}
 			else {
 				
