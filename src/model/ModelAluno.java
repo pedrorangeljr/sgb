a7ab12834f42aeaddd7a7c4f94bc8c169aff0b04
pedrorangeljr <pedro.curtis.rangel@gmail.com>
2022-05-24 17:41:21 -0300

    model aluno

---
+++ src/model/ModelAluno.java
@@ -0,0 +1,20 @@
+package model;
+
+import java.io.Serializable;
+
+public class ModelAluno implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long idAluno;
+	private String nome;
+	private String telefone;
+	private String cpf;
+	private String cep;
+	private String logradouro;
+	private String numero;
+	private String bairro;
+	private String cidade;
+	private String uf;
+
+}
